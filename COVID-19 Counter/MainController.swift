b57a638a87ff79837ce32c248d653ad215e35b16
Alejandro Hernandez <alex.hernandezc.21@gmail.com>
2024-08-26 19:47:55 -0600

    Add localizable warning Increase marketing version number Remove unnecessary code

--- COVID-19 Counter/MainController.swift
+++ COVID-19 Counter/MainController.swift
@@ -11,20 +11,16 @@ class MainController: UIViewController, UITextFieldDelegate {
     
 // MARK: - IBOutlets
     
-    @IBOutlet weak var descriptionLbl: UILabel!
     @IBOutlet weak var addBtn: UIButton!
     @IBOutlet weak var minusBtn: UIButton!
     @IBOutlet weak var resetBtn: UIButton!
-    @IBOutlet weak var newLimitBtn: UIButton!
-    @IBOutlet weak var counterPicker: UIPickerView!
     @IBOutlet weak var currentValueLbl: UILabel!
     @IBOutlet weak var currentValueBkgView: UIView!
     
     
     
 // MARK: - Variables and constants
-    let counter = CounterLogic()
-    var totalCapacity: Double = 0
+    var currentValue = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,27 +28,17 @@ class MainController: UIViewController, UITextFieldDelegate {
     }
     
     private func configureView() {
-        
-        /*let labels: [UILabel] = [separatorCapacityLbl, counterCapacityLbl, capacityLimitLbl]
-        for label in labels {
-            label.font = UIFont(name: FONT_HEAVY, size: 46)
-            label.numberOfLines = 1
-            label.adjustsFontSizeToFitWidth = true
-            label.minimumScaleFactor = 0.5
-            label.textColor = .white
-        }*/
-        
         addBtn.setImage(UIImage(systemName: "plus"), for: .normal)
         addBtn.setTitle("", for: .normal)
         addBtn.tintColor = .white
         addBtn.setPreferredSymbolConfiguration(UIImage.SymbolConfiguration(pointSize: 32, weight: .regular), forImageIn: .normal)
-        addBtn.backgroundColor = UIColor(named: "GreenColor")
+        addBtn.backgroundColor = UIColor(named: "customGreen")
         addBtn.layer.cornerRadius = 12
         
         minusBtn.setImage(UIImage(systemName: "minus"), for: .normal)
         minusBtn.setTitle("", for: .normal)
         minusBtn.tintColor = .white
-        minusBtn.backgroundColor = UIColor(named: "RedColor")
+        minusBtn.backgroundColor = UIColor(named: "customRed")
         minusBtn.setPreferredSymbolConfiguration(UIImage.SymbolConfiguration(pointSize: 32, weight: .regular), forImageIn: .normal)
         minusBtn.layer.cornerRadius = 12
         
@@ -60,96 +46,50 @@ class MainController: UIViewController, UITextFieldDelegate {
         resetBtn.setTitle("", for: .normal)
         resetBtn.tintColor = .white
         resetBtn.setPreferredSymbolConfiguration(UIImage.SymbolConfiguration(pointSize: 30, weight: .regular), forImageIn: .normal)
-        resetBtn.backgroundColor = UIColor(named: "TealColor")
+        resetBtn.backgroundColor = UIColor(named: "customTeal")
         resetBtn.layer.cornerRadius = 12
         
-        currentValueLbl.text = "232"
+        currentValueLbl.text = "\(currentValue)"
         currentValueLbl.font = UIFont(name: FONT_HEAVY, size: 64)
         
         currentValueBkgView.backgroundColor = UIColor(named: "BackgroundViewColor")
         currentValueBkgView.layer.cornerRadius = 12
-        currentValueBkgView.layer.shadowColor = UIColor.lightGray.cgColor
-        currentValueBkgView.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
-        currentValueBkgView.layer.shadowRadius = 12.0
-        currentValueBkgView.layer.shadowOpacity = 0.5
+//        currentValueBkgView.layer.shadowColor = UIColor.lightGray.cgColor
+//        currentValueBkgView.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+//        currentValueBkgView.layer.shadowRadius = 12.0
+//        currentValueBkgView.layer.shadowOpacity = 0.5
         
-        /*newLimitBtn.setImage(UIImage(systemName: "plus"), for: .normal)
-        newLimitBtn.tintColor = .black
-        ne LimitBtn.setPreferredSymbolConfiguration(UIImage.SymbolConfiguration(pointSize: 28, weight: .regular), forImageIn: .normal)
-        newLimitBtn.setTitle("", for: .normal)*/
-        
-        /*descriptionLbl.text = ""
-        descriptionLbl.font = UIFont(name: FONT_MEDIUM, size: 24)
-        descriptionLbl.textAlignment = .center*/
+    }
+    
+    private func presentInvalidNumberAlert() {
+        let alert = UIAlertController(title: NSLocalizedString("zeroWarningTitle", comment: "") , message: NSLocalizedString("zeroWarning", comment: ""), preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(alert, animated: true, completion: nil)
     }
     
 // MARK: -IBActions
     @IBAction func addBtnPressed(sender: UIButton) {
         sender.pulse()
-        /*switch counter.checkCapacityLimit() {
-        case false:
-            let result = counter.incrementCounter()
-            descriptionLbl.text = result.backgroundInfo.message
-            counterCapacityLbl.text = String(format: "%.0f", result.currentCapacity)
-        default:
-            let alert = UIAlertController(title: NSLocalizedString("noLimitAlertTitle", comment: ""), message: NSLocalizedString("noLimitAlertMessage", comment: ""), preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
-            present(alert, animated: true, completion: nil)
-        }*/
+        currentValue += 1
+        currentValueLbl.text = "\(currentValue)"
     }
     
     @IBAction func minusBtnPressed(sender: UIButton) {
         sender.pulse()
-        /*switch counter.checkCapacityLimit() {
-        case false:
-            let result = counter.decrementCounter()
-            descriptionLbl.text = result.backGroundInfo.message
-            counterCapacityLbl.text = String(format: "%.0f", result.currentCapacity)
-        default:
-            let alert = UIAlertController(title: NSLocalizedString("noLimitAlertTitle", comment: ""), message: NSLocalizedString("noLimitAlertMessage", comment: ""), preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
-            present(alert, animated: true, completion: nil)
+        currentValue -= 1
+        if currentValue < 0 {
+            presentInvalidNumberAlert()
+            currentValue = 0
+        } else {
+            currentValueLbl.text = "\(currentValue)"
         }
-         */
+
     }
 
-    @IBAction func newLimitBtnPressed(sender: UIButton) {
-        sender.pulse()
-        /*let newLimitAlert = UIAlertController(title: NSLocalizedString("newLimitAlertTitle", comment: ""), message: NSLocalizedString("newLimitAlertMessage", comment: ""), preferredStyle: .alert)
-
-        let setAction = UIAlertAction(title: NSLocalizedString("newLimitAlertSet", comment: ""), style: .default) { (alert) in
-            let textField = newLimitAlert.textFields![0] as UITextField
-            let textStr = textField.text ?? "0"
-            self.capacityLimitLbl.text = textStr
-            self.totalCapacity = Double(textStr) ?? 0
-            self.counterCapacityLbl.text = "0"
-            self.backgroundVw.backgroundColor = greenColor
-            
-            self.counter.getTotalCapacity(capacity: self.totalCapacity)
-            print("TextField is:", textStr)
-            print("Finished")
-        }
-        
-        newLimitAlert.addAction(setAction)
-        
-        newLimitAlert.addTextField { (textfield) in
-            setAction.isEnabled = false
-            NotificationCenter.default.addObserver(forName: UITextField.textDidChangeNotification, object: textfield, queue: OperationQueue.main) { (_) in
-                let textCount = textfield.text?.trimmingCharacters(in: .whitespacesAndNewlines).count ?? 0
-                let textIsNotEmpty = textCount > 0
-                setAction.isEnabled = textIsNotEmpty
-            }
-            textfield.placeholder = NSLocalizedString("newLimitPlaceholder", comment: "")
-            textfield.keyboardType = .numberPad
-        }
-
-        newLimitAlert.addAction(UIAlertAction(title: NSLocalizedString("newLimitAlertCancel", comment: ""), style: .cancel, handler: nil))
-
-        present(newLimitAlert, animated: true, completion: nil)*/
-    }
-    
     @IBAction func resetBtnPressed(_ sender: UIButton) {
         sender.pulse()
+        currentValue = 0
+        currentValueLbl.text = "\(currentValue)"
     }
 }
 
